5b8e5b63c3ab34974b437c5b72a3ecd333f3b2a7
dtirnanic <dejan.tirnanic85@gmail.com>
2020-02-17 08:50:58 -0600

    Superhero model class created with properties to represent attributes and db colums

---
+++ Models/Superhero.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Superhero.Models
+{
+    public class Superhero
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string AlterEgo { get; set; }
+        public string PrimaryAbility { get; set; }
+        public string SecondaryAbility { get; set; }
+        public string CatchPrase { get; set; }
+    }
+}
