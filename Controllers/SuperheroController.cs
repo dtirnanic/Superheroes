0782f0aaa6393e2a7bb4f7c20fc05c9a50b8d661
dtirnanic <dejan.tirnanic85@gmail.com>
2020-02-17 09:51:14 -0600

    Created a SuperheroController/scaffolding with read/write privileges, created view folder Superhero and view file for Create method in scaffolding.

---
+++ Controllers/SuperheroController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Superhero.Controllers
+{
+    public class SuperheroController : Controller
+    {
+        // GET: CRUD
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CRUD/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CRUD/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CRUD/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CRUD/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CRUD/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CRUD/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CRUD/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
